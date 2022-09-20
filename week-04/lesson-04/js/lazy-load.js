5bc52e6bddb01e95ca27815c2af9bbf4cdb6254f
= <=>
2022-09-20 19:59:29 +0200

    Completed Lazy Load Activity

---
+++ week-04/lesson-04/js/lazy-load.js
@@ -0,0 +1,68 @@
+// Obtain all the image Elements in the Document
+let pendingImages = document.querySelectorAll('img[data-src');
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: '0px 0px -1000px 0px'
+};
+
+// Create a function to replace and remove the data-src attribute with an src attribute upon loading an image
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+        image.removeAttribute('data-src');
+    };
+};
+
+// Loop through images and load them
+pendingImages.forEach((img) => {
+    loadImages(img);
+});
+
+// Create a condition to display the real image when it comes into the viewport, otherwise, display the placeholder.
+// if ('IntersectionObserver' in window) {
+//     const observer = new IntersectionObserver((items, observer) => {
+//       items.forEach((item) => {
+//         if (item.isIntersecting) {
+//           loadImages(item.target);
+//           observer.unobserve(item.target);
+//         }
+//       });
+//     });
+//     pendingImages.forEach((img) => {
+//       observer.observe(img);
+//     });
+//   } else {
+//     pendingImages.forEach((img) => {
+//       loadImages(img);
+//     });
+//   }
+
+// Alternate way to do the above:
+
+const images = document.querySelectorAll('[data-src]'); // Putting anything in square brackets locates attributes, even in CSS.
+
+const preloadImage = (img) => {
+    const imgSrc = img.getAttribute('data-src');
+    if (!imgSrc) {
+        return;
+    }
+
+    img.src = imgSrc;
+}
+
+const imageView = new IntersectionObserver((entries, imageView) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        }
+        else {
+            preloadImage(entry.target);
+            imageView.unobserve(entry.target);
+        }
+    });
+}, imgOptions);
+
+images.forEach(image => {
+    imageView.observe(image);
+});

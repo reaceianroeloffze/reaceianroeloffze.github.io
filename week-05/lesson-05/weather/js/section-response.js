e13d6c17edb0d5d1200dedf7a1ccea6610b752cc
= <reaceianroeloffze@gmail.com>
2022-11-23 15:48:21 +0200

    Adding Town Data to Homepage

--- week-05/lesson-05/weather/js/section-response.js
+++ week-05/lesson-05/weather/js/section-response.js
@@ -1,14 +1,31 @@
 
 // Create a toggle function for expanding/collapsing sections on a webpage.
-const sectionToggle = () => {
+const storyToggle = () => {
+    document.querySelector('.story-toggle').classList.toggle('expand');
     document.querySelector('.weather-article').classList.toggle('expand');
     document.querySelector('#weather-story').classList.toggle('expand');
-    // document.getElementById('hazards').classList.toggle('expand');
+}
+
+const hazardToggle = () => {
+    document.getElementById('hazards').classList.toggle('expand');
+    // document.getElementById('hazards').classList.toggle('expand-large');
+    document.querySelector('.hazard-toggle').classList.toggle('expand');
+    document.querySelector('.veld-fire').classList.toggle('expand');
+    // document.querySelector('.veld-fire').classList.toggle('expand-large');
+    document.querySelector('.heat-wave').classList.toggle('expand');
+    // document.querySelector('.heat-wave').classList.toggle('expand-large');
+}
+
+const infoToggle = () => {
     // document.querySelector('#town-info').classList.toggle('expand');
-    document.querySelector('.section-toggle').classList.toggle('expand');
+    document.querySelector('.info-toggle').classList.toggle('expand');
 }
 
 // Set the applicable CSS to be applied once the menu button is clicked.
-const sectionBtn = document.querySelector('.section-toggle');
+const storyBtn = document.querySelector('.story-toggle');
+const hazardBtn = document.querySelector('.hazard-toggle');
+const infoBtn = document.querySelector('.info-toggle');
 
-sectionBtn.onclick = sectionToggle;
+storyBtn.onclick = storyToggle;
+hazardBtn.onclick = hazardToggle;
+infoBtn.onclick = infoToggle;

5f4ed4bc1df25a2aed56681debf0fdb4e9f304dc
Reace Ian Roeloffze <reaceianroeloffze@gmail.com>
2023-02-23 08:57:30 +0200

    Addind Scoots Site Plan to Repo

---
+++ week-06/project/scoots/main-site/js/date.js
@@ -0,0 +1,12 @@
+const options1 = {
+    year: 'numeric'
+};
+
+document.querySelector('.copyright__year').textContent = new Date().toLocaleDateString('en-gb', options1);
+
+const options2 = {
+    dateStyle: 'short'
+};
+
+let update = document.querySelector('.last-updated__date').textContent = new Date(document.lastModified).toLocaleDateString('en-za', options2);
+
